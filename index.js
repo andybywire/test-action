12ffa4ada404027fdf79b609fc7f06a32d33bef6
Andy Fitzgerald <andy@andyfitzgeraldconsulting.com>
2021-11-02 09:32:11 -0700

    Revert "Add sanity client and test"
    This reverts commit d606da25c20597f799901afd3513ee7ff8df1f04.

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-const sanityClient = require('@sanity/client');
-const client = sanityClient({
-  projectId: "4g5tw1k0",
-  dataset: "production",
-  apiVersion: '2021-03-25',
-  useCdn: false, // `false` ensures fresh data
-  withCredentials: false
-});
-
-
 try {
 
-  const query = '*[_type == "method"'
-  client.fetch(query).then((methodData) => {
-    console.log(JSON.stringify(methodData, null, 2))
-  })
+  async function fetchSanityData() {
+    const query = "https://4g5tw1k0.api.sanity.io/v2021-03-25/data/query/production?query=*%5B_type%20%3D%3D%20%24type%5D%7B%0A%20%20title%2C%0A%20%20_id%2C%0A%20%20%22methodId%22%3A%20slug.current%2C%0A%20%20%22inputs%22%3A%20transputReference.inputsReference%5B%5D-%3E%7B%0A%20%20%20%20prefLabel%2C%0A%20%20%20%20%22inputId%22%3A%20conceptId.current%0A%20%20%7D%2C%0A%20%20%22outputs%22%3A%20transputReference.outputReference%5B%5D-%3E%7B%0A%20%20%20%20prefLabel%2C%0A%20%20%20%20%22outputId%22%3A%20conceptId.current%0A%20%20%7D%0A%7D&%24broader=%22transputTaxonomy_Recommendation%22&%24pageSlug=%22card-sorting%22&%24type=%22method%22"
+    const response = await fetch(query);
+    const groqQuery = await response.json();
+    console.log(groqQuery);
+  }
+  fetchSanityData();
 
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
