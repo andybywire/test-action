cb49b78dd48457088d8b1399c467a1360bcc10c2
Andy Fitzgerald <andy@andyfitzgeraldconsulting.com>
2021-11-02 09:30:32 -0700

    Add test async fetch function

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+const sanityClient = require('@sanity/client');
+const client = sanityClient({
+  projectId: "4g5tw1k0",
+  dataset: "production",
+  apiVersion: '2021-03-25',
+  useCdn: false, // `false` ensures fresh data
+  withCredentials: false
+});
+
+
 try {
+
+  const query = '*[_type == "method"'
+  client.fetch(query).then((methodData) => {
+    console.log(JSON.stringify(methodData, null, 2))
+  })
+
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
